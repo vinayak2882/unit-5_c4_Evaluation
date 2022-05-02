b9846758adc3ac2926cd20b9edd4993b3bda43b5
Vinayak Patrut <96100321+vinayak2882@users.noreply.github.com>
2022-05-02 22:44:49 +0530

    Update Logout.jsx

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -5,10 +5,7 @@ import { logout } from "../Redux/AuthRed/actions";
 import { store } from "../Redux/store";
 
 export const Logout = () => {
-   // Logout component, just log user out and take him to `/` homepage
-
-   // suggestion: if you are storing anyting in redux it's a good idea to
-   // empty it before loggin out. eg: order
+ 
    const dispatch = useDispatch();
    useSelector((store) => {
       console.log(store);
